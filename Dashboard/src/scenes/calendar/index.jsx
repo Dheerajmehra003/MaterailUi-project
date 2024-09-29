3fa6d962ea17a279ab3598f991313a9ecb717a5f
Dheerajmehra003 <dheerajsinghmehra0405@gmail.com>
2024-09-29 17:08:16 +0530

    calendar & form

---
+++ Dashboard/src/scenes/calendar/index.jsx
@@ -0,0 +1,91 @@
+import { useState } from "react";
+import { Calendar , formatDate} from '@fullcalendar/core';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import timeGridPlugin from '@fullcalendar/timegrid';
+import interactionPlugin from '@fullcalendar/interaction'
+import listPlugin from '@fullcalendar/list';
+import { useTheme } from "@emotion/react";
+import { tokens } from "../../theme";
+import { Box, List, ListItem, ListItemText, Typography } from "@mui/material";
+import Header from "../../components/Header";
+
+const FullCalendar = () => {
+    const theme = useTheme();
+    const colors = tokens(theme.palette.mode);
+    const [currentEvents, setCurrentEvents] = useState([]);
+
+    const handleDateClick = (selected) => {
+        const title = prompt("Please enter a new title for your event");
+        const calendarApi = selected.view.calendar;
+        calendarApi = unselect();
+
+        if(title) {
+            calendarApi.addEvent({
+                id: `${selected.date}-${title}`,
+                title,
+                start: selected.startStr,
+                end: selected.endStr,
+                allDay: selected.allDay,
+            })
+        }
+    }
+
+    const handleEventClick = (selected) => {
+     if(
+     window.confirm(
+        `Are you sure you want to delete the event '${selected.event.title}'`
+     )
+    ) {
+      selected.event.remove();
+      }
+    };
+
+    return (
+        <>
+        <Box m="20px">
+        <Header title='CALENDAR' subtitle='Full Calendar Interative Page' />
+
+        <Box display="flex" justifyContent="space-between">
+            {/* calendar sidebar */}
+            <Box 
+            flex='1 1 20%'
+            bgcolor={colors.primary[400]}
+            p="15px"
+            borderRadius="4px"
+            >
+            <Typography variant="h5">Events</Typography>
+             <List>
+              {currentEvents.map((event) =>(
+                <ListItem 
+                key={event.id}
+                sx={{
+                    bgcolor:colors.greenAccent[500],
+                    margin:"10px 0",
+                    borderRadius:"2px"
+                }}
+                >
+                 <ListItemText
+                 primary={event.title}
+                 secondary={
+                    <Typography>
+                        {formatDate(event.start, {
+                            year: "numeric",
+                            month: "short",
+                            day: "numeric",
+                        })}
+                    </Typography>
+                 }
+                 />                
+                </ListItem>
+              ))}
+             </List>
+
+            </Box>
+
+        </Box>
+        </Box>
+        </>
+    )
+}
+
+export default FullCalendar;
